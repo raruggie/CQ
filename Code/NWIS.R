779f8bc645aa676b824bd120fdfd207a5656ea95
raruggie <ryrugg80@gmail.com>
2023-11-07 11:33:28 -0500

    NWIS

--- Code/NWIS.R
+++ Code/NWIS.R
@@ -10,6 +10,8 @@ library(ggpubr)
 library(ggnewscale)
 library(ggforce)
 library(readxl)
+library(MASS)
+library(car)
 library(CropScapeR)
 library(FedData)
 library(streamstats)
@@ -1975,23 +1977,31 @@ lapply(c(1:4), \(i) make_plot(i))
  ####~~~~ MLR ~~~~####
 #######################
 
+save(df.cor, df.OLS_Sens, file = 'Processed_Data/troubleshoot_AIC.Rdata')
+
 # create the l..cor list as above but now only filter out the 
 # non-significant attributes:
+
+load('Processed_Data/troubleshoot_AIC.Rdata')
 
 l.cor.MLR<-df.cor %>%
   split(., df.cor$CQ_Parameter)%>%
   lapply(., \(i) i%>% 
            arrange(Spearman_Correlation)%>%  
-           slice_head(n = 7)%>%
-           bind_rows(i%>%arrange(desc(Spearman_Correlation))%>%slice_head(n = 7))%>%
-           distinct(term, .keep_all = T)%>%
-           filter(!between(Spearman_Correlation, -0.25,.25))%>%
+           # slice_head(n = 7)%>%
+           # bind_rows(i%>%arrange(desc(Spearman_Correlation))%>%slice_head(n = 7))%>%
+           # distinct(term, .keep_all = T)%>%
+           # filter(!between(Spearman_Correlation, -0.25,.25))%>%
            mutate(sig_0.05 = factor(sig_0.05, levels = c('not', 'sig')))%>%
-           filter(sig_0.05 == 'sig')%>%
-           mutate(term = factor(term, levels = unique(term[order(Spearman_Correlation)])))) # Order by correlation strength
+           # mutate(term = factor(term, levels = unique(term[order(Spearman_Correlation)])))%>%
+           # filter(sig_0.05 == 'sig')%>%
+           as.data.frame())
 
-# create a list of dataframes from ____ and subseting the attributes using the 
-# names in each l.cor[[i]]$term:
+
+x<-l.cor.MLR[[1]]
+
+# create a list of dataframes from df.OLS.Sens and subseting the attributes using the 
+# names in each l.cor[[i]]$term for simple and full models:
 
 l.cor.MLR.full<-lapply(1:4, \(i) df.OLS_Sens%>%
                     select(i+1, l.cor.MLR[[i]]$term)%>%
@@ -2000,38 +2010,79 @@ l.cor.MLR.full<-lapply(1:4, \(i) df.OLS_Sens%>%
 
 x<-l.cor.MLR.full[[1]]
 
-# create a list of lm objects from eachdataframe:
+# loop through data frames and make lm and stepAIC objects (wont work on lapply for some reason):
 
-l.cor.MLR.m<-lapply(l.cor.MLR.full, \(i) lm(term~., i))
+# set up list for aic objects to append into:
 
-x<-l.cor.MLR.m[[1]]
+l.aic<-list()
 
-summary(x)
-
-names(l.cor.MLR.m)<-names(df.OLS_Sens)[2:5]
-
-# create a list of stepAIC:
-
-library(MASS)
-library(car)
-
-l.cor.AIC<-lapply(l.cor.MLR.m, \(i) stepAIC(i, direction = 'both'))
-
+# loop:
 
 i<-1
-x<-stepAIC(l.cor.MLR.m[[i]])
+
+for (i in 1:4){
+  
+  # find the best predictor name for the simple model:
+  
+  n<-l.cor.MLR[[i]]$term[which.max(abs(l.cor.MLR[[i]]$Spearman_Correlation))]
+  
+  # createformula with this predictor:
+  
+  n<-paste('term ~', n)
+  
+  # create simple model:
+  
+  m.simple<-lm(n,l.cor.MLR.full[[i]])
+  
+  # create full model:
+  
+  m.full<-lm(term~., l.cor.MLR.full[[i]])
+  
+  # create AIC object:
+  
+  aic<-stepAIC(m.simple, scope = list(upper=m.full, lower =~1), direction = 'both', trace = FALSE)
+
+  # save aic object to list:
+  
+  l.aic[[i]]<-aic
+  
+}
+
+# look at summary of aic objects:
+
+lapply(l.aic, summary)
+
+# set names of list elements:
+
+names(l.aic)<-names(df.OLS_Sens)[2:5]
+
+names(l.aic[[1]]$model)[-1]
+  
+# extract best predictors for each parameter:
+
+l<-lapply(l.aic, \(i) names(i$model)[-1])
+
+# look at univariate plots of these. to do this:
+
+# make a list of ggplot objects for each parameter:
+
+l.MLR.plots<-lapply(1:4, \(i) df.OLS_Sens%>%
+                       select(i+1, l[[i]])%>%
+                       as.data.frame()%>%
+                      pivot_longer(cols = 2:last_col(), names_to = 'Attribute', values_to = 'value')%>%
+                      mutate(Attribute = factor(Attribute, levels = l[[i]]))%>%
+                      ggplot(., aes(y = !!sym(names(df.OLS_Sens)[i+1]), x = value))+
+                      facet_wrap('Attribute', scales = 'free')+
+                      geom_point()+
+                      geom_smooth(method = 'lm')+
+                      ggtitle((names(df.OLS_Sens)[i+1]))
+                       
+                    
+                    )
+
+l.MLR.plots
 
 #
-
-summary()
-
-
-
-
-
-
-
-
 
 
 
